7939f173294015ab0507b45c4e14229486652cd9
ChidinmaKO <kchidinmavictoria@gmail.com>
2018-03-06 18:35:07 +0100

    Fresh start

--- day00.js
+++
@@ -1,8 +0,0 @@
-function processData(inputString) {
-    // This line of code prints the first line of output
-
-    console.log("Hello, World.\nWelcome to 30 Days of Code!");
-
-    // Write the second line of output that prints the contents of 'inputString' here.
-}
-
