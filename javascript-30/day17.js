2c94013585a6f26076fa118a07270de67e352b03
ChidinmaKO <kchidinmavictoria@gmail.com>
2018-04-02 07:28:12 +0200

    more exceptions

---
+++ javascript-30/day17.js
@@ -0,0 +1,15 @@
+//Write your code here
+function Calculator(){
+    this.power = function(n, p){
+        try{
+            if (n >= 0 && p >= 0){
+                return Math.pow(n, p);
+            } else{
+                throw 'n and p should be non-negative';
+            }
+        }
+        catch(e){
+            return e;
+        }
+    }
+}
