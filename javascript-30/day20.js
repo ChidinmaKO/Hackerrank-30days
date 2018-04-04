b30cc5553be4f3eeec2d34180fdc77b18ed44d52
ChidinmaKO <kchidinmavictoria@gmail.com>
2018-04-04 15:33:46 +0200

    sorting (bubble sorting)

---
+++ javascript-30/day20.js
@@ -0,0 +1,47 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    a = readLine().split(' ');
+    a = a.map(Number);
+    // Write Your Code Here
+    let swapped;
+    let numSwaps = 0;
+
+    do{
+        swapped = false;
+        for(let i = 0; i < n - 1; i++){
+            if(a[i] > a[i + 1]){
+                let temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+                swapped = true;
+                numSwaps++;
+            }
+        }
+    } while(swapped === true);
+        console.log("Array is sorted in " + numSwaps + " swaps.");
+        console.log("First Element: " + a[0]);
+        console.log("Last Element: " + a[n-1]);
+
+}
