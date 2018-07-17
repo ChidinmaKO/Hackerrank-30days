1fa3e45229da3cd98fd615d75124c03d64abe0bb
ChidinmaKO <kchidinmavictoria@gmail.com>
2018-07-17 14:01:00 +0200

    Weather Observation Station 18

--- sql/45.sql
+++ sql/45.sql
@@ -2,3 +2,4 @@ SELECT ROUND(ABS((MIN(lat_n) - MAX(lat_n))) + ABS((MIN(long_w) - MAX(long_w))), 
 FROM station;
 --mysql & oracle
 --here I learnt about the ABS() function
+-- I used it to calculate the manhattan distance between two points |a-c|+|b-d|
