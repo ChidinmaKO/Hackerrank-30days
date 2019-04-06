f1fd1841b1afdf978a93a35ea6562152c32ce400
ChidinmaKO <kchidinmavictoria@gmail.com>
2019-04-06 21:08:07 +0200

    mod divmod

---
+++ practice-python/11.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+
+# integer division
+print(a//b)
+# mod 
+print(a%b)
+# divmod
+print (divmod(a,b))
+
